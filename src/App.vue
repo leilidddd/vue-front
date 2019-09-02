5478b74f505146e67cd109f8621cbc28f52f04fa
leili <476371581@qq.com>
2019-09-02 11:39:41 +0800

    AAa

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<ul>
+  <li v-for="(user) in users" :key="user.name">
+    {{user.name}}
+  </li>
+</ul>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      users:[]
+    }
+  },
+  mounted(){
+    axios.get('http://localhost:3000/api/users').then(response=>{
+      this.users=response.data;
+    })
+  }
+}
+</script>
+
+<style>
+</style>
